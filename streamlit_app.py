0dc0a1a4d416354f6a93e811c958b562ba4ac55f
Bert Rico <142132680+b-rico@users.noreply.github.com>
2024-10-03 23:38:24 -0500

    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
@@ -17,7 +18,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name')),col('search_on')
 
-#pd_df = my_dataframe.to_pandas()
+pd_df = pd.DataFrame(my_dataframe)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
@@ -31,7 +32,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = my_dataframe.loc[my_dataframe['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
+        search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
